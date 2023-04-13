884914af4009f0d9151ac61cb32c30ac95b2ba72
Dwight Caddell <dcaddelljr@gmail.com>
2023-04-13 11:02:22 -0500

    JavaScriptVariables

--- app.js
+++ app.js
@@ -3,8 +3,44 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
 
-// Exercise 2
+const firstName = "Dwight";
+let lastName = "Caddell";
+var age = 42;
 
-// Exercise 3
+//firstName = "Dwight"
+lastName = "Caddell"
+age = 42;
+
+var exercise1And2 = `Hello ${firstName} ${lastName}. You are ${age} years old.`
+
+console.log(exercise1And2)
+
+let language = "JavaScript";
+let createdYear = 1995
+let isCaseSensitive = true;
+
+var exercise3 = `${language} was created in ${createdYear}.  It is ${isCaseSensitive} that ${language} is case sensitive.`
+
+console.log(exercise3)
+
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M"
+
+var exercise4 = `I saw an ad in the paper saying that the ${selectedSize} shirt for ${price} was ${salesPercentage}% off, but found out that was ${isOnSale}. I was told the ${selectedSize} shirts had a stock of ${stock} and I replied with ${inStock}!`
+
+console.log(exercise4)
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmakr = 456;
+let hasRead = true;
+
+var exercise5 = `It is ${hasRead} that I have read the book ${title}. The author is ${author} and the book has ${pageCount} pages. I found the bookmark I had been looking for stuffed on page ${bookmakr}.`
+
+console.log(exercise5)
